97b69bc94a7720ef173798c54928ab3f2b57a40d
arthur <arthur.brandao.ext@provu.com.br>
2023-01-04 15:05:51 -0300

    Changing webdriver as service

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from time import sleep
 
 url = 'https://clubedogol.com/'
-chromedriver = ChromeDriverManager().install()
+chromedriver = Service(ChromeDriverManager().install())
 
-driver = webdriver.Chrome(chromedriver)
+driver = webdriver.Chrome(service=chromedriver)
 
 driver.get(url)
 
@@ -16,5 +17,10 @@ search_input.send_keys('clube do gol g3')
 search_button = driver.find_element(By.XPATH, '//button[text()="Pesquisar"]')
 search_button.click()
 
-sleep(5)
-driver.close()
+#
+#
+# sleep(500)
+
+#TODO: checar se o elemento apareceu na tela: //a[@title='Clube do Gol G3']
+
+# driver.close()
